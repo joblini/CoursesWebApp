b84c6c2ced3d8fe1e9cf11b1d336f94f545256af
Ingo Jobling <ingo.jobling@gmail.com>
2023-07-02 14:39:13 -0400

    First commit

---
+++ Models/DataAccessController.cs
@@ -0,0 +1,27 @@
+﻿namespace CoursesWebApp.Models
+{
+    public class DataAccessController
+    {
+        // TODO: Add your connection string in the following statements
+        private string connectionString = "<Azure SQL Database Connection String>";
+
+        // Retrieve all details of courses and their modules    
+        public IEnumerable<CoursesAndModules> GetAllCoursesAndModules()
+        {
+            List<CoursesAndModules> courseList = new List<CoursesAndModules>();
+
+            // TODO: Connect to the database
+            //using ()
+            {
+                // TODO: Specify the SQL query to run
+                
+                // TODO: Execute the query
+                
+                // TODO: Read the data a row at a time
+                
+                // TODO: Close the database connection
+            }
+            return courseList;
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoursesWebApp.Pages;
+
+public class CoursesAndModulesModel : PageModel
+{
+    private readonly ILogger<CoursesAndModulesModel> _logger;
+
+    public CoursesAndModulesModel(ILogger<CoursesAndModulesModel> logger)
+    {
+        _logger = logger;
+    }
+
+    // TODO: Create a DataAccessController object
+
+    // TODO: Create a collection for holding CoursesAndModules object
+
+    public void OnGet()
+    {
+            // TODO: Retrieve the data using the DataAccessController object and populate the CoursesAndModules object
+
+    }
+}
